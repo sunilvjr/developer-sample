12d7a8b8becec5791f40d276eaa5f7bbab83a28b
sunilvjr <32095252+sunilvjr@users.noreply.github.com>
2023-11-30 12:30:57 -0500

    Changes

--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -4,8 +4,27 @@ namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+        public static int GetFactorial(int n) 
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * GetFactorial(n - 1);
+            }
+        }
 
-        public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
+        public static string FormatSeparators(params string[] items) {
+           
+            if (items == null || items.Length == 0)
+            {
+                return string.Empty;
+            }
+            string separator = ",";
+            string result = string.Join(separator, items);
+            return result;
+        }
     }
 }
